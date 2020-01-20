a70c093577dd3f115e5221e118797c04243ceadb
Jean-Phi-ben <59736465+Jean-Phi-ben@users.noreply.github.com>
2020-01-20 22:35:29 +0100

    Add files via upload

---
+++ lib/04_simon_says.rb
@@ -0,0 +1,16 @@
+def echo(string)
+    puts "#{string}"
+end
+
+def shout(sting)
+    puts "#{string.upcase}"
+end
+
+
+def repeat(string, num)
+    num.times do print "#{string} " end
+end
+
+def start_of_word(string)
+    puts #{string.chars.first}
+end
